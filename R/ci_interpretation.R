5eec5913e90cc0986f71a223a721899b34ed99f2
Ingo Rohlfing <ingo.rohlfing@uni-passau.de>
2024-06-02 20:06:27 +0200

    Add calculation and distribution for upper mu

--- R/ci_interpretation.R
+++ R/ci_interpretation.R
@@ -11,7 +11,14 @@ ui <- fluidPage(
                   step = 0.01, round = 2),
       br(),
       textOutput("lower_p_value_output"),
-      textOutput("lower_critical_value")
+      textOutput("lower_critical_value"),
+      br(),
+      sliderInput("upper_mu", "Upper population mean",
+                  min = 22, max = 28, value = 28,
+                  step = 0.01, round = 2),
+      br(),
+      textOutput("upper_p_value_output"),
+      textOutput("upper_critical_value")
     ),
     mainPanel(
       plotOutput("obs_plot")
@@ -28,16 +35,39 @@ server <- function(input, output) {
     sample_sd <- 15
     sample_size <- 27
     standard_error <- sample_sd / sqrt(sample_size)
-    lower_p_value <- 2*round(
-      pnorm(q = sample_mean, mean = input$lower_mu, sd = standard_error,
-                     lower.tail = FALSE), digits = 3)
-    lower_critical_value <- round(
-      qnorm(p = 0.975, mean = input$lower_mu, sd = standard_error), digits = 3)
+    p_value_calculation <- function(mu, lower_tail) {
+      p_value <- 2*round(
+        pnorm(q = sample_mean, mean = mu, sd = standard_error,
+              lower.tail = lower_tail), digits = 3)
+      return(p_value)
+    }
+    lower_p_value <- p_value_calculation(input$lower_mu, lower_tail = FALSE)
+    upper_p_value <- p_value_calculation(input$upper_mu, lower_tail = TRUE)
+    critical_value_calculation <- function(mu, lower_tail) {
+      critical_value <- round(
+        qnorm(p = 0.025, mean = mu, sd = standard_error, lower.tail = lower_tail),
+        digits = 3)
+      return(critical_value)
+    }
+    lower_critical_value <- critical_value_calculation(input$lower_mu,
+                                                       lower_tail = FALSE)
+    upper_critical_value <- critical_value_calculation(input$upper_mu,
+                                                       lower_tail = TRUE)
     output$lower_p_value_output <- renderText(
-      paste("p-value of sample mean: ", lower_p_value)
+      paste("p-value of sample mean for ", input$lower_mu, ": ", lower_p_value,
+            sep = "")
       )
+    output$upper_p_value_output <- renderText(
+      paste("p-value of sample mean for ", input$upper_mu, ": ", upper_p_value,
+            sep = "")
+    )
     output$lower_critical_value <- renderText(
-      paste("Critical value: ", lower_critical_value)
+      paste("Critical value for ", input$lower_mu, ": ", lower_critical_value,
+            sep = "")
+    )
+    output$upper_critical_value <- renderText(
+      paste("Critical value for ", input$upper_mu, ": ", upper_critical_value,
+            sep = "")
     )
   })
 
@@ -62,7 +92,11 @@ server <- function(input, output) {
                                            mean = input$lower_mu,
                                            sd = standard_error)),
                    color = "blue", linetype = "dashed") +
-      geom_vline(aes(xintercept = sample_mean), color = "black") +
+      geom_segment(aes(x = input$upper_mu, xend = input$upper_mu,
+                       y = 0, yend = dnorm(x = input$upper_mu,
+                                           mean = input$upper_mu,
+                                           sd = standard_error)),
+                   color = "red", linetype = "dashed") +
       stat_function(
         fun = dnorm, geom = "line", color = "blue",
         xlim = c(9, qnorm(0.975, mean = input$lower_mu, sd = standard_error)),
@@ -70,15 +104,27 @@ server <- function(input, output) {
       ) +
       stat_function(
         fun = dnorm, geom = "area", fill = "blue", alpha = 0.5,
-        xlim = c(qnorm(0.975, mean = input$lower_mu, sd = standard_error), 27),
+        xlim = c(qnorm(0.975, mean = input$lower_mu, sd = standard_error), 31),
         args = list(mean = input$lower_mu, sd = standard_error)
       ) +
-      scale_x_continuous("Mean", limits = c(9, 27), breaks = seq(9, 27, 1)) +
+      stat_function(
+        fun = dnorm, geom = "line", color = "red",
+        xlim = c(qnorm(0.025, mean = input$upper_mu, sd = standard_error), 31),
+        args = list(mean = input$upper_mu, sd = standard_error),
+      ) +
+      stat_function(
+        fun = dnorm, geom = "area", fill = "red", alpha = 0.5,
+        xlim = c(9, qnorm(0.025, mean = input$upper_mu, sd = standard_error)),
+        args = list(mean = input$upper_mu, sd = standard_error)
+      ) +
+      scale_x_continuous("Mean", limits = c(9, 31), breaks = seq(9, 31, 1)) +
       scale_y_continuous("Density",
                          labels = seq(0, 0.15, by = 0.05),
                          breaks = seq(0, 0.15, by = 0.05)) +
-      labs(caption = paste("Lower bound: ", round(lower_bound, digits = 2),
-                           "\nUpper bound: ", round(upper_bound, digits = 2))) +
+      labs(caption = paste("Lower bound of confidence interval",
+                           round(lower_bound, digits = 2),
+                           "\nUpper bound of confidence interval: ",
+                           round(upper_bound, digits = 2))) +
       theme_classic()
   })
 }
