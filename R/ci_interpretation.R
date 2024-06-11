b703ccd2f923c3187e675c85ab322ef8e10bec60
Ingo Rohlfing <ingo.rohlfing@uni-passau.de>
2024-06-11 14:40:24 +0200

    Add repo link, update README

--- R/ci_interpretation.R
+++ R/ci_interpretation.R
@@ -21,7 +21,10 @@ ui <- fluidPage(
                   min = 22, max = 28, value = 28,
                   step = 0.01, round = 2),
       textOutput("upper_p_value_output"),
-      textOutput("upper_critical_value")
+      textOutput("upper_critical_value"),
+      br(),
+      p(a('GitHub repository',
+          href = 'https://github.com/ingorohlfing/CI-plausible-values'))
     ),
     mainPanel(
       card(
@@ -50,29 +53,7 @@ ui <- fluidPage(
              at the top of the panel.
              *Enjoy*!")
       )
-    ) #,
-  # bslib::card(
-  #   markdown("**Explanation**: The plot illustrates one possible interpretation
-  #            of the 95% confidence interval for means that is different from the
-  #            *95-percent-of-confidence-intervals-contain-the-true-mean* interpretation.
-  #            In the alternative interpretation, the confidence interval is the
-  #            range of population means for which the sample mean does *not* achieve
-  #            statistical significence at the 0.05 level in a two-sided test
-  #            (assuming it is the 95% confidence interval). \n
-  #            The left, blue dashed line represents the population mean \u03bc
-  #            that can be set with one slider. For the chosen level of \u03bc,
-  #            the *upper-critical* value for the blue sampling distribution is
-  #            calculated and shown as
-  #            the blue shaded area in the right tail. The displayed p-value is
-  #            the p-value of the sample mean for the chosen population mean \u03bc
-  #            for a two-sided test and a level of \u03b1 of 0.05. \n
-  #            When the population mean equal to the lower bound of the
-  #            confidence interval, the sample mean is exactly the right-tailed
-  #            critical value and the p-value equals 0.05. A similar
-  #            interpretation applies to the right-hand sampling
-  #            distribution. \n
-  #            *Enjoy*!")
-  # )
+    )
   )
 )
 # Define server logic
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## README
 
 This is a Shiny project illustrating the meaning of the confidence interval
-for means. This is work in progress.
+for means. More information about the interpretation of the confidence interval
+and what the app does can be found in the app.  
+
+
